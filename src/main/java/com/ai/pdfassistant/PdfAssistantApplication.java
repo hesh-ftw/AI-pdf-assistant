1cb2f85d90afbf984cfd6d29d6f913ab00beaa84
Chamath Heshan <chamathheshan076@gmail.com>
2024-08-25 09:43:38 +0530

    initial commit

---
+++ src/main/java/com/ai/pdfassistant/PdfAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.ai.pdfassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfAssistantApplication.class, args);
+    }
+
+}
